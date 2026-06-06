a923e6069ab2d2f1b75ed99a1b003c15eb15654b
Zeshan Rasul <ZeshanRasul@users.noreply.github.com>
2026-06-06 13:15:57 +0100

    Implement Gauss Elimination with partial pivoting and solve tridiagonal systems of equations

--- LinearAlgebra/GaussElimination/gausspivot.m
+++ LinearAlgebra/GaussElimination/gausspivot.m
@@ -1,20 +1,26 @@
-close all; clear all; clc;
-function x = GaussNaive(A,b)
-
+close all, clear all, clc;
+function x = GuassPivot(A, b)
     [m, n] = size(A);
-    if m~=n, error ('Matrix A must be square'); end
-    
+    if m ~=n, error('Matrix A must be square'); end
+
     nb = n+1;
     Aug = [A b];
-    
+
     % Forward Elimination
     for k = 1:n-1
+        %Partial Pivoting
+        [big, i] = max(abs(Aug(k:n, k)));
+        ipr = i + k - 1;
+        if ipr ~= k
+            Aug([k, ipr], :) = Aug([ipr, k],:);
+        end
+        
         for i = k+1:n
             factor = Aug(i, k) / Aug(k, k);
             Aug(i, k:nb) = Aug(i, k:nb) - factor * Aug(k, k:nb);
         end
     end
-    
+
     % Back Substitution
     x = zeros(n, 1);
     x(n) = Aug(n, nb) / Aug(n, n);
@@ -24,11 +30,9 @@ function x = GaussNaive(A,b)
     end
 end
 
-A = [3 -0.1 -0.2; 0.1 7 -0.3; 0.3 -0.2 10];
-b = [7.85; -19.3; 71.4];
+A = [0.0003 3.0000; 1.0000 1.0000];
+b = [2.0001; 1.0000];
 
-x = GaussNaive(A, b);
-
-% Display the solution
+x = GuassPivot(A, b);
 disp('The solution vector x is:');
 disp(x);
